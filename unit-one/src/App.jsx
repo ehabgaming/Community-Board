0f4d86c47320828ddae809a78259ba4a72e8a5e2
ehabgaming <ehabothman2005@gmail.com>
2026-06-05 22:59:50 -0400

    Add Card component and update App to display Community Board

--- unit-one/src/App.jsx
+++ unit-one/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Card from "./Components/Card.jsx";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Community Board</h1>
+      <Card title="First Card" description="This is the first card." />
+    </div>
+  );
 };
 
 export default App;
---
+++ unit-one/src/Components/Card.jsx
@@ -0,0 +1,10 @@
+const Card = (props) => {
+  return (
+    <div className="card">
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </div>
+  );
+};
+
+export default Card;
